5f2ed155422526ee5e8645e4e0e3bd81e71bfeb1
mjuribe <105825647+mjuribe@users.noreply.github.com>
2022-06-05 12:00:15 -0500

    Excepciones en el manejo de archivos
    Solo se modifica repositorioDatos.py y se actualiza el archivo de texto de pendientes

--- repositorioDatos.py
+++ repositorioDatos.py
@@ -13,33 +13,48 @@ class RepositorioDatos:
     return self.nombreArchivoNodos
   
   def obtenerPuntosControl(self):
-    #TODO: manejar excepciones
-    with open(self.nombreArchivoPuntosControl, 'r') as archivo:
-      lineas = csv.reader(archivo)
-      puntosControl = self.convertirLineasAPuntos(lineas)
+    try:
+      with open(self.nombreArchivoPuntosControl, 'r') as archivo:
+        lineas = csv.reader(archivo)
+        puntosControl = self.convertirLineasAPuntos(lineas)
+    except FileNotFoundError:
+      print("No se encuentra el archivo con los puntos de control.")
+      exit()
     return puntosControl
   
   def obtenerNodos(self):
-    #TODO: manejar excepciones
-    with open(self.nombreArchivoNodos, 'r') as archivo:
-      lineas = csv.reader(archivo)
-      knots = self.convertirLineasALista(lineas)
+    try:
+      with open(self.nombreArchivoNodos, 'r') as archivo:
+        lineas = csv.reader(archivo)
+        knots = self.convertirLineasALista(lineas)
+    except FileNotFoundError:
+      print("No se encuentra el archivo con los nodos.")
+      exit()
     return knots
 
   def convertirLineasALista(self, lineas):
     lista = []
     for linea in lineas:
-      #TODO: manejar excepciones
-      lista.append(float(linea[0]))
+      try:
+        lista.append(float(linea[0]))
+      except ValueError:
+        print("Los nodos deben ser únicos y de caracter numérico.")
+        exit()
     return lista
   
   def convertirLineasAPuntos(self, lineas):
     puntos = []
     for linea in lineas:
-        #TODO: manejar excepciones
-        x = float(linea[0])
-        y = float(linea[1])
-        puntos.append([x, y])
+        try:
+          x = float(linea[0])
+          y = float(linea[1])
+          puntos.append([x, y])
+        except IndexError:
+          print("No se encuentran solo dos puntos de control por línea.")
+          exit()
+        except ValueError:
+          print("Los puntos de control deben ser de caracter numérico.")
+          exit()
     return puntos
 
 
