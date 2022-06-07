125d70ad03735c985e9decb178c3a01061b2ed72
mjuribe <105825647+mjuribe@users.noreply.github.com>
2022-06-06 20:13:27 -0500

    Input grado b-spline

--- pruebasBoor.py
+++ pruebasBoor.py
@@ -3,16 +3,11 @@ import matplotlib.pyplot as plt
 from repositorioDatos import RepositorioDatos
 from boor import Boor
 
-#Borrar
-import numpy as np
-import math as m
-
 repo = RepositorioDatos()
 controlPoints = repo.obtenerPuntosControl()
 nodos = repo.obtenerNodos()
 
-#TODO: Pedir al usuario por teclado o similar
-gradoBSpline = 3
+gradoBSpline = int(input("Grado del B-Spline: "))
 
 boor = Boor(controlPoints, nodos, gradoBSpline)
 (xs, ys) = boor.calcularPuntos()
